7269bdd28a58c0e3cc2cff03f423b421609b04ee
prosperoliverc124-byte <prosperoliverc124@gmail.com>
2026-03-22 04:27:07 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector('.bubbles');
-
-for (let i = 0; i < 20; i++) {
-  const bubble = document.createElement('div');
-  bubble.classList.add('bubble');
-  bubble.style.left = Math.random() * 100 + 'vw';
-  bubble.style.animationDuration = (5 + Math.random() * 5) + 's';
-  container.appendChild(bubble);
-}
